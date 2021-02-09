5f840b01da1815e8fd42aaecac54282c04ed13c2
qorg11 <qorg@vxempire.xyz>
2021-02-09 18:39:15 +0100

    Never used that function

--- clainsafecli/clainsafecli.h
+++ clainsafecli/clainsafecli.h
@@ -7,9 +7,6 @@ static write_data(void *buffer, size_t size, size_t nmemb,
 void
 print_usage();
 
-int progress_func(void* ptr, double TotalToDownload, double NowDownloaded, 
-	double TotalToUpload, double NowUploaded);
-
 int
 store_link(const char *path, const char *buf);
 
